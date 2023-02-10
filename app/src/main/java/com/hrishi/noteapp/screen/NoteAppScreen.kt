e56c8398c8ad5298332ad2c05d8967ea29cb7972
kingslayer1312 <hrishabari@gmail.com>
2023-02-10 20:06:28 +0530

    Set up different modules

---
+++ app/src/main/java/com/hrishi/noteapp/screen/NoteAppScreen.kt
@@ -0,0 +1,21 @@
+package com.hrishi.noteapp.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteAppScreen() {
+    Column(modifier = Modifier.padding(6.dp)) {
+        
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteAppScreenPreview() {
+    NoteAppScreen()
+}
